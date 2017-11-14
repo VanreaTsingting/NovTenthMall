a42b57460de11b1dc7e8fa5f5cb7dc9d05099ac2
YuanXIaoWen <15521325541@163.com>
2017-11-14 20:43:13 +0800

    foreign

--- src/view/HelloWorld.vue
+++ src/view/HelloWorld.vue
@@ -1,34 +1,72 @@
 <template>
-  <div class="hello">
+  <div class="container" style="background-color: rgb(235,232,232)">
 
-    <!--header-->
+    <!--1. header-->
     <div class="header">
-      <nav>
-        <ul>
-          <li v-for="(item, index) in tab_list" v-bind:class="{tab_logo:index==0}">
-            <a href="" v-if="index==0"><img src="https://raw.githubusercontent.com/github/explore/6c6508f34230f0ac0d49e847a326429eefbfc030/topics/html/html.png">{{ item.name }}</a>
-            <a href="" v-if="index!=0&&index!=tab_list.length-1">{{ item.name }} |</a>
-            <a href="" v-if="index==tab_list.length-1">{{ item.name }}</a>
-          </li>
-        </ul>
-      </nav>
+      <div style="padding-left: 100px; padding-right: 60px;" >
+        <div v-for="(item, index) in tab_list">
+          <a style="float: left;color: #333333" href="" v-if="index==0">
+            <img style="width:134px;height:126px;"
+                 src="http://www.gemmy.com.cn/ZH_CN/images/logo.png"></a>
+          <a style="float: right;padding-top: 15px;color: #333333" href="" v-if="index!=0&&index!=1">{{ item.name }} |</a>
+          <a style="float: right;padding-top: 15px;color: #333333" href="" v-if="index==1">{{ item.name }}</a>
+        </div>
+      </div><br/>
+
+
     </div>
 
-    <!--content-->
-    <div class="content">
-      <div class="content_nav">
-        <ul>
-          <li v-for="item in tab_list"  @mouseleave="tabLeave" @mouseenter="tabEnter(item)">
-            <a @click="tabbtnclick(item)">{{item.name}}</a>
-          </li>
-        </ul>
-        <div class="popupWindowS" v-show="showFlag"><img width="60px" height="60px" src="https://i1.mifile.cn/a4/xmad_14982250861854_iDbOz.jpg"></div>
-      </div
-      ><div class="content_main"></div>
+    <!--1. header-->
+    <div style="margin-top: 60px;">
+      <div style="background-color: #094;">
+        <div style="padding: 10px 80px 10px 80px;text-align: center;" >
+          <div style="display: inline-block;margin-right: 10px;" v-for="(item, index) in tab_list">
+            <a style="margin-right: 60px" href="" v-if="index==0">{{ item.name }}</a>
+            <a href="" v-if="index!=0&&index!=1">{{ item.name }} |</a>
+            <a href="" v-if="index==1">{{ item.name }}</a>
+          </div>
+        </div>
+      </div>
+
+
     </div>
 
-    <!--footer-->
-    <div class="footer"></div>
+
+
+    <div>
+      <!--2. left-->
+      <div class="left">
+        <div>1导航</div>
+        <div>2导航</div>
+        <div>3导航</div>
+        <div>4导航</div>
+      </div>
+
+      <!--3. right-->
+      <div class="right">
+        <div>sss</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>sss</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>sss</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>sss</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>sss</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+      </div>
+    </div>
+    <div></div>
 
   </div>
 </template>
@@ -41,40 +79,30 @@ export default {
       msg: 'Welcome to Your Vue.js App',
       tab_list: [
         {name: 'xx电商', tab_flag: true},
-        {name: '首页', tab_flag: false},
-        {name: '走进xxx', tab_flag: false},
-        {name: '山泉水知识', tab_flag: false},
-        {name: '产品介绍', tab_flag: false},
-        {name: '促销优惠', tab_flag: false},
-        {name: '客户服务', tab_flag: false},
-        {name: '加入我们', tab_flag: false},
+        {name: '用户设置', tab_flag: false},
         {name: '精彩分享', tab_flag: false},
-        {name: '用户设置', tab_flag: false}
+        {name: '加入我们', tab_flag: false},
+        {name: '客户服务', tab_flag: false},
+        {name: '促销优惠', tab_flag: false},
+        {name: '产品介绍', tab_flag: false},
+        {name: '山泉水知识', tab_flag: false},
+        {name: '走进xxx', tab_flag: false},
+        {name: '首页', tab_flag: false}
       ],
       showFlag: false
     }
   },
   methods: {
-    btnclick () {
-      this.$ajax({
-        method: 'post',
-        url: '/user',
-        data: {
-          name: 'wise',
-          info: 'wrong'
-        }
-      })
-    },
     tabbtnclick (item) {
       console.log('Tab Click：' + item.name)
     },
     tabLeave () {
       console.log('tabLeave')
-      this.showFlag = false
+//      this.showFlag = false
     },
     tabEnter (item) {
       console.log('tabEnter' + item.name)
-      this.showFlag = true
+//      this.showFlag = true
     }
   }
 }
@@ -82,63 +110,41 @@ export default {
 
 <style scoped>
 
-body .header{
-  display:block;
-  background:#66CC00;
-  height:50px;
-  width:100%;
-  padding:10px 0px 0px 0px;
-  margin:0px;
-  position:fixed;
+
+.container {
+  overflow: hidden;
+  height: 100%;
 }
 
-
-.header nav ul li.tab_logo{
-  background:#66CC00;
-  list-style:none;
-  float:left;
-  font-size: 18px;
+.left {
+  width: 15%;
+  margin-left: 5%;
+  float: left;
+  clear:both;
+  background-color: #e2e2e2;
+  min-height: 100%;
+  margin-bottom: -99999px;
+  padding: 0 0 99999px;
 }
 
-.header nav ul li{
-  background:#66CC00;
-  display:inline-block;
-  list-style:none;
-  float:right;
-  color: #ffffff;
-}
-
-.header nav ul li.tab_logo img{
-  width:40px;
-  height:40px;
-}
-
-.content{
+.left div {
+  border-bottom: 1px solid #ccc;
+  color: #6f6f6f;
   display: block;
-  text-align:center;
+  padding: 20px 2px;
+  text-decoration: none;
 }
 
-.content .content_nav{
-  background: #06d688;
-  width:15%;
-  height: 8880px;
-  display: inline-block;
+.right {
+  width: 75%;
+  margin-right: 5%;
+  float: left;
+
 }
 
-.content .content_main{
-  margin: auto;
-  background: #83d65e;
-  width:85%;
-  height: 80px;
-  display: inline-block;
-}
-
-.content_nav ul li{
-  list-style: none;
-}
-
-.popupWindowS{
-
+.right div {
+  height: 100px;
+  background-color: #ddd;
 }
 
 </style>
